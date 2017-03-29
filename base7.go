209eae7ee420e319a87697c6986a5a426c203062
fupeng <peng.fu@baishancloud.com>
2017-03-29 23:35:43 +0800

    fix: 添加base7说明

--- base7.go
+++ base7.go
@@ -1,3 +1,12 @@
+//Given an integer, return its base 7 string representation.
+// Example 1:
+// Input: 100
+// Output: "202"
+
+// Example 2:
+// Input: -7
+// Output: "-10"
+
 package main
 
 import (
