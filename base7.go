4b29bc92ea8babef2b7980915001ea4ea5f414a7
fupeng <peng.fu@baishancloud.com>
2017-03-29 23:35:01 +0800

    feat: add base 7

---
+++ base7.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func convertToBase7(num int) string {
+
+	//没有do while, 只能先判断
+	if num == 0 {
+		return "0"
+	}
+	var result string
+	tmp := num
+	if num < 0 {
+		tmp = -num
+	}
+	// result = 求余 + result
+	for tmp > 0 {
+		result = strconv.Itoa(tmp%7) + result
+		tmp /= 7
+	}
+	if num < 0 {
+		result = "-" + result
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(convertToBase7(100))
+	fmt.Println(convertToBase7(-7))
+	fmt.Println(convertToBase7(0))
+}
