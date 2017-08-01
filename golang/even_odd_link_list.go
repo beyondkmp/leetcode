1ccbe72916d9a3519bdbfd0feb80b70f2eda46b0
beyondkmp <beyondkmp@gmail.com>
2017-08-02 00:40:18 +0800

    fix: 优化代码

--- golang/even_odd_link_list.go
+++ golang/even_odd_link_list.go
@@ -11,12 +11,12 @@ type ListNode struct {
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	var head *ListNode
-	var tmp_head = new(ListNode)
-	head = tmp_head
+	var tmpHead = new(ListNode)
+	head = tmpHead
 	for _, v := range a {
 		node := ListNode{Val: v, Next: nil}
-		tmp_head.Next = &node
-		tmp_head = &node
+		tmpHead.Next = &node
+		tmpHead = &node
 	}
 	result := oddEvenList(head.Next)
 	for ; result != nil; result = result.Next {
@@ -27,42 +27,19 @@ func main() {
 func oddEvenList(head *ListNode) *ListNode {
 	if head == nil {
 		return nil
-
-	}
-	var odd_head *ListNode
-	var odd_last *ListNode
-	var even_head *ListNode
-	var even_last *ListNode
-
-	odd_head = head
-	odd_last = odd_head
-
-	if head.Next != nil {
-		even_head = head.Next
-		even_last = head.Next
-
-	} else {
-		return head
-
 	}
 
-	var tmp_head *ListNode
-	tmp_head = even_head.Next
-	i := 3
-	for ; tmp_head != nil; tmp_head, i = tmp_head.Next, i+1 {
-		if i%2 == 0 {
-			even_last.Next = tmp_head
-			even_last = tmp_head
+	oddLast := head
+	evenHead := head.Next
+	evenLast := evenHead
 
-		} else {
-			odd_last.Next = tmp_head
-			odd_last = tmp_head
-
-		}
-
+	for evenLast != nil && evenLast.Next != nil {
+		oddLast.Next = evenLast.Next
+		oddLast = oddLast.Next
+		evenLast.Next = oddLast.Next
+		evenLast = evenLast.Next
 	}
-	odd_last.Next = even_head
-	even_last.Next = nil
-	return odd_head
+	oddLast.Next = evenHead
 
+	return head
 }
