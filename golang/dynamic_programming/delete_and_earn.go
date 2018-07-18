269256c9de97ef42962fb988c5d511016d731978
beyondkmp <beyondkmp@gmail.com>
2018-07-18 11:55:27 +0800

    optimize for deleteAndEarn

--- golang/dynamic_programming/delete_and_earn.go
+++ golang/dynamic_programming/delete_and_earn.go
@@ -12,12 +12,14 @@ func max(a, b int) int {
 func deleteAndEarn(nums []int) int {
 	values := make([]int, 10001)
 
+	maxN := 0
 	for _, v := range nums {
 		values[v] += v
+		maxN = max(maxN, v)
 	}
 
 	var take, skip int
-	for i := 0; i < 9; i++ {
+	for i := 0; i < maxN+1; i++ {
 		takei := skip + values[i]
 		skipi := max(take, skip)
 
