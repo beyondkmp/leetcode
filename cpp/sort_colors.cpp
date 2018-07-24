0982c686a29e499ed87f1b7eb1368b70d0bcfa0f
beyondkmp <beyondkmp@gmail.com>
2018-07-24 20:19:00 +0800

    format cpp

--- cpp/sort_colors.cpp
+++ cpp/sort_colors.cpp
@@ -1,43 +1,34 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    void sortColors(vector<int>& nums)
-    {
-        int low, mid, high;
-        low = mid = 0;
-        high = nums.size() -1;
+  void sortColors(vector<int> &nums) {
+    int low, mid, high;
+    low = mid = 0;
+    high = nums.size() - 1;
 
-        while (mid <= high)
-        {
-            if (nums[mid] == 0)
-            {
-                swap(nums[low++], nums[mid++]);
-            }
-            else if (nums[mid] == 1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums[mid], nums[high--]);
-            }
-        }
+    while (mid <= high) {
+      if (nums[mid] == 0) {
+        swap(nums[low++], nums[mid++]);
+      } else if (nums[mid] == 1) {
+        mid++;
+      } else {
+        swap(nums[mid], nums[high--]);
+      }
     }
+  }
 };
 
-int main()
-{
-    vector<int> nums{0, 1, 2, 1, 2, 0};
-    Solution s = Solution();
-    s.sortColors(nums);
-    for (auto i : nums)
-        cout << i << " ";
-    cout << endl;
+int main() {
+  vector<int> nums{0, 1, 2, 1, 2, 0};
+  Solution s = Solution();
+  s.sortColors(nums);
+  for (auto i : nums)
+    cout << i << " ";
+  cout << endl;
 
-    return 1;
+  return 1;
 }
