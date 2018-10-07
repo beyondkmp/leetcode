f74b824bea1926b0aa6e0bacdbf749b370fdc864
beyondkmp <beyondkmp@gmail.com>
2018-10-07 12:43:50 +0800

    optimize scala Solution for largestNumber

--- scala/largest_number.scala
+++ scala/largest_number.scala
@@ -1,12 +1,15 @@
 object Solution {
   def largestNumber(nums: Array[Int]): String = {
-    val s = nums.sortWith((a,b)=> a.toString + b.toString > b.toString+a.toString).mkString
-    if (s.startsWith("0")) "0" else s
+    val s =
+      nums.sortWith((a, b) => a.toString + b.toString > b.toString + a.toString)
+    if (s(0) == 0) "0" else s.mkString
   }
 
-  def main(args: Array[String]): Unit ={
-    val a = Array(1,2,2,34)
+  def main(args: Array[String]): Unit = {
+    val a = Array(1, 2, 2, 34)
     println(largestNumber(a))
+
+    val b = Array(0, 0)
+    println(largestNumber(b))
   }
 }
-
