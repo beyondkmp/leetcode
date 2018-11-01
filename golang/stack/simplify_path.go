3e9cb77b2ce3eccc5dbe7e72b85223b69bab8d52
beyondkmp <beyondkmp@gmail.com>
2018-11-01 19:53:49 +0800

    add golang solution for simplifyPath

---
+++ golang/stack/simplify_path.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func simplifyPath(path string) string {
+	var tmp []rune
+	var result []string
+
+	for _, s := range path + "/" {
+		if s == '/' {
+			switch string(tmp) {
+			case "":
+			case ".":
+			case "..":
+				if len(result) > 0 {
+					result = result[:len(result)-1]
+				}
+			default:
+				result = append(result, string(tmp))
+			}
+			tmp = nil
+		} else {
+			tmp = append(tmp, s)
+		}
+	}
+	return "/" + strings.Join(result, "/")
+}
+
+func main() {
+	fmt.Println(simplifyPath("/a/./b/../../c/"))
+	fmt.Println(simplifyPath("/..."))
+}
