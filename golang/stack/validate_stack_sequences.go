519ec28bc97b69ebb36ed25eba89f3a59a2c7c62
beyondkmp <beyondkmp@gmail.com>
2018-12-17 00:21:11 +0800

    add solution for validateStackSequences

---
+++ golang/stack/validate_stack_sequences.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type stack []int
+
+func (s stack) Empty() bool {
+	return len(s) == 0
+}
+
+func (s stack) Peek() int {
+	return s[len(s)-1]
+}
+
+func (s *stack) Put(i int) {
+	(*s) = append((*s), i)
+}
+
+func (s *stack) Pop() int {
+	d := (*s)[len(*s)-1]
+	(*s) = (*s)[:len(*s)-1]
+	return d
+}
+
+func validateStackSequences(pushed []int, popped []int) bool {
+	var st stack
+	var p int
+	for _, v := range pushed {
+		st.Put(v)
+		for !st.Empty() && p < len(popped) && st.Peek() == popped[p] {
+			st.Pop()
+			p++
+		}
+	}
+
+	if st.Empty() {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	pushed := []int{1, 0}
+	popped := []int{1, 0}
+	fmt.Println(validateStackSequences(pushed, popped))
+
+}
