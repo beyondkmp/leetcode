5f28261f35a8bc274cf2042c63ff9a3c49c9ba7f
beyondkmp <beyondkmp@gmail.com>
2019-01-02 05:59:33 +0800

    add brute force solution for numRescueBoats

---
+++ golang/greed/num_rescue_boats.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type byWeight []int
+
+func (w byWeight) Len() int           { return len(w) }
+func (w byWeight) Less(i, j int) bool { return w[i] > w[j] }
+func (w byWeight) Swap(i, j int)      { w[i], w[j] = w[j], w[i] }
+
+func numRescueBoats(people []int, limit int) int {
+	flag := make([]bool, len(people))
+	result := 0
+	var i, j int
+
+	sort.Sort(byWeight(people))
+	for i = 0; i < len(people); i++ {
+		if flag[i] {
+			continue
+		}
+		for j = i + 1; j < len(people); j++ {
+			if !flag[j] && people[i]+people[j] <= limit {
+				flag[j] = true
+				result++
+				break
+			}
+		}
+		if j >= len(people) {
+			flag[i] = true
+			result++
+		}
+	}
+	return result
+}
+
+func main() {
+	people := []int{3, 5, 3, 4}
+	limit := 5
+	fmt.Println(numRescueBoats(people, limit))
+}
