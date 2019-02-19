034a8abe9c8148cbc265fb59cbda9a75515f4f3e
beyondkmp <beyondkmp@gmail.com>
2019-02-19 19:02:19 +0800

    use copy

--- golang/backtracking/combinations.go
+++ golang/backtracking/combinations.go
@@ -10,12 +10,11 @@ func combine(n int, k int) [][]int {
 	visitAll = func(pos int, start int) {
 		if pos == k {
 			tmp := make([]int, k)
-			for j := 0; j < k; j++ {
-				tmp[j] = nums[j]
-			}
+			copy(tmp, nums)
 			results = append(results, tmp)
 			return
 		}
+
 		for i := start; i <= n; i++ {
 			nums[pos] = i
 			visitAll(pos+1, i+1)
@@ -25,6 +24,7 @@ func combine(n int, k int) [][]int {
 	visitAll(0, 1)
 	return results
 }
+
 func main() {
 	fmt.Println(combine(5, 3))
 }
