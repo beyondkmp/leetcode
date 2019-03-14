dfd6756a28d1251607b428153f3da5e6a35b06c3
beyondkmp <beyondkmp@gmail.com>
2019-03-15 07:22:48 +0800

    add solution for pathSum2

---
+++ golang/tree/path_sum2.go
@@ -0,0 +1,34 @@
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) [][]int {
+	var result [][]int
+	var dfs func(r *TreeNode, sum int, sol []int)
+
+	dfs = func(r *TreeNode, sum int, sol []int) {
+		if r == nil || sum < 0 {
+			return
+		}
+
+		if r.Left == nil && r.Right == nil && sum == r.Val {
+			tmp := make([]int, len(sol)+1)
+			copy(tmp, sol)
+			tmp = append(tmp, r.Val)
+			result = append(result, tmp)
+			return
+		}
+
+		sol = append(sol, r.Val)
+		dfs(r.Left, sum-r.Val, sol)
+		dfs(r.Right, sum-r.Val, sol)
+	}
+
+	dfs(root, sum, nil)
+	return result
+}
